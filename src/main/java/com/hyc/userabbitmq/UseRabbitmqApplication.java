ed9b49725d4a9aeedb7691c70ad90b063f1bb4e3
huangyc <hyc2613@163.com>
2019-08-26 18:10:47 +0800

    初始化springboot工程

---
+++ src/main/java/com/hyc/userabbitmq/UseRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.hyc.userabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseRabbitmqApplication.class, args);
+    }
+
+}
